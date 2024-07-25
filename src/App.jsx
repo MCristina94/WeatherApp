6371c1306389bd2fa3aad367bc39bff97a8bdab7
Maureen Garcia <cmaureen1908@gmail.com>
2024-07-25 14:50:22 -0500

    dev with material ui in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,137 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { LoadingButton } from "@mui/lab";
+import { Box, Container, TextField, Typography } from "@mui/material"
+import { useState } from "react";
 
+console.log(import.meta.env.VITE_API_KEY);
+const API_WEATHER = `https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&q=`
 function App() {
-  const [count, setCount] = useState(0)
+console.log(API_WEATHER);
+  const [city, setCity] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({
+    error: false,
+    message: "",
+  });
+  const [weather, setWeather] = useState({
+    city: "",
+    country: "",
+    temp: "",
+    condition: "",
+    icon: "",
+    conditionText: ""
+  });
 
+  const onSubmit = async(e)=>{
+    e.preventDefault();
+    setLoading(true)
+    setError({
+      error: false,
+      message: "",
+    })
+    try {
+      if(!city.trim()) throw {message: "El campo ciudad es obligatorio"};
+      
+      const response = await fetch(`${API_WEATHER}${city}`);
+      const data = await response.json();
+      
+      if(data.error) throw {message: data.error.message};
+
+      setWeather({
+        city: data.location.name,
+        country: data.location.country,
+        temp: data.current.temp_c,
+        condition: data.current.condition.code,
+        icon: data.current.condition.icon,
+        conditionText: data.current.condition.text,
+      });
+      
+      console.log(data);
+    } catch (error) {
+      console.error(error)
+      setError({
+        error: true,
+        message: error.message,
+      })
+    } finally {
+      setLoading(false);
+    }
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <Container 
+      maxWidth= 'xs' sx={{mt:2}} 
+    >
+     <Typography
+      variant="h3"
+      component="h1"
+      align="center"
+      gutterBottom
+     >Weather App</Typography>
+     <Box
+      sx={{display: "grid", gap: 2}}
+      component="form"
+      autoComplete="off"
+      onSubmit={onSubmit}
+     >
+      <TextField
+        id="city"
+        label="Ciudad"
+        variant="outlined"
+        size="small"
+        required
+        fullWidth
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        error={error.error}
+        helperText={error.message}
+      />
+      <LoadingButton
+        type="submit"
+        variant="contained"
+        loading={loading}
+        loadingIndicator="Cargando..."
+      >
+        Buscar
+      </LoadingButton>
+     </Box>
+     {weather.city && (
+      <Box
+        sx={{
+          mt: 2,
+          display: "grid",
+          gap: 2,
+          textAlign: "center"
+        }}
+      >
+        <Typography variant="h4" component="h2">
+          {weather.city}, {weather.country}
+        </Typography>
+        <Box
+          component="img"
+          alt={weather.conditionText}
+          src={weather.icon}
+          sx={{margin: "0 auto"}}
+          />
+        <Typography variant="h5" component="h3">
+          {weather.temp} °C
+        </Typography>
+        <Typography variant="h6" component="h4">
+          {weather.conditionText}
+        </Typography>
+      </Box>
+     )}
+     <Typography
+        textAlign="center"
+        sx={{ mt: 2, fontSize: "10px" }}
+      >
+        Powered by:{" "}
+        <a
+          href="https://www.weatherapi.com/"
+          title="Weather API"
+        >
+          WeatherAPI.com
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </Typography>
+    </Container>
   )
 }
 
